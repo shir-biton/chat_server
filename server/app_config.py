ab4a8c2daa2ff9173e92aff22811978d2079bdfc
Shir Biton <shirbi10@gmail.com>
2024-01-17 13:18:41 +0200

    Fixed settings

--- server/app_config.py
+++ server/app_config.py
@@ -1,3 +1,5 @@
+import os
+
 from pydantic_settings import BaseSettings
 
 
@@ -6,7 +8,7 @@ class Settings(BaseSettings):
     db_password: str = "postgres"
     db_user: str = "postgres"
     db_name: str = "chat_db"
-    db_host: str = "localhost"
+    db_host: str = os.getenv("DB_HOST", "localhost")
     db_url: str = f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 
